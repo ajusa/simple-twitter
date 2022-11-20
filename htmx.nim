1b02d697f892b4189b22b4bf553870311344d53d
Arham Jain <arhamjain@gmail.com>
2022-11-20 18:04:44 -0500

    Move to pigeon and taggy

---
+++ htmx.nim
@@ -0,0 +1,13 @@
+import taggy
+
+proc hxGet*(value: string) = attr "hx-get", value
+proc hxPost*(value: string) = attr "hx-post", value
+proc hxTarget*(value: string) = attr "hx-target", value
+proc hxSwap*(value: string) = attr "hx-swap", value
+proc hxExt*(value: string) = attr "hx-ext", value
+proc hxSwapOob*(value: bool) = attr "hx-swap-oob", $value
+proc hxVars*[T](pairs: openArray[(string, T)]) =
+  var finalValue = ""
+  for (key, value) in pairs:
+    finalValue &= key & ":" & $value & ","
+  attr "hx-vars", finalValue
--- server.nim
+++ server.nim
@@ -1,5 +1,5 @@
-import strutils, sugar, sequtils
-import jester, norm/[model, sqlite], nimja
+import strutils, sugar
+import taggy, htmx, pigeon, norm/[model, sqlite]
 
 type Post* = ref object of Model
   text*: string
@@ -7,48 +7,44 @@ type Post* = ref object of Model
 let db = open(":memory:", "", "", "")
 db.createTables(Post())
 
-let BLANK_TWEET_FORM = """
-  <form id="newtweet" hx-swap-oob="true">
-    <div><textarea placeholder="Write here" name="text"></textarea></div>
-    <button hx-post="/posts" hx-target="#posts" hx-swap="beforeend">Add</button>
-  </form>"""
+proc blankTweetForm() = form "#newtweet":
+  hxSwapOob true
+  tdiv: textarea: placeholder "Write here"; name "text"
+  button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
 
-proc render(post: Post): string =
-  if not post.isNil:
-    compileTemplateStr """
-    <form hx-target="this">
-      <span>{{post.text}}</span>
-      <button hx-get="/posts/edit/{{post.id}}">Edit</button>
-      <button hx-delete="/posts/{{post.id}}">Delete</button>
-    </form>""" 
+proc view(post: Post) = form:
+  hxTarget "this"
+  hxVars {"id": post.id}
+  span: say post.text
+  button: hxGet "/getEditPost"; say "Edit"
+  button: hxPost "/removePost"; say "Delete"
 
-proc renderEdit(post: Post): string = compileTemplateStr """
-  <textarea placeholder="Write here" name="text">{{post.text}}</textarea>
-  <button hx-get="/posts/{{post.id}}">Cancel</button>
-  <button hx-put="/posts/{{post.id}}">Update</button>""" 
+{.push gcsafe.}
+autoRoute:
+  GET "/"; proc index(): string = render: html:
+    let posts = @[Post()].dup(db.select("1"))
+    lang "en"
+    head:
+      meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
+      script: src "https://unpkg.com/htmx.org@1.6.1"
+      script: src "https://unpkg.com/htmx.org@1.8.4/dist/ext/json-enc.js"
+      title: say "Simple Twitter"
+    body:
+      hxExt "json-enc"
+      tdiv "#posts":
+        for post in posts: post.view()
+      blankTweetForm()
+  proc getPost(id: int): string = render Post().dup(db.select("id = ?", id)).view()
+  proc posts(text: string): string = render:
+    blankTweetForm(); Post(text: text).dup(db.insert).view()
+  proc updatePost(id: int, text: string): string = render:
+    Post(id: id, text: text).dup(db.update).view()
+  proc removePost(id: int) = discard Post(id: id).dup(db.delete)
+  proc getEditPost(id: int): string = render:
+    let post = Post().dup(db.select("id = ?", id))
+    textarea: placeholder "Write here"; name "text"; say post.text
+    button: hxGet "/getPost"; say "Cancel"
+    button: hxPost "/updatePost"; say "Update"
+{.pop gcsafe.}
 
-proc index(posts: seq[Post]): string = compileTemplateStr """
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8", name="viewport", content="width=device-width, initial-scale=1">
-      <script src="https://unpkg.com/htmx.org@1.6.1"></script>
-      <title>Simple Twitter</title>
-    </head>
-    <body>
-      <div id="posts">{{posts.mapIt(it.render).join}}</div>
-      {{BLANK_TWEET_FORM}}
-    </body>
-  </html>"""
-
-proc parsePost(request: Request): Post = 
-  result.id = request.params.getOrDefault("id", "0").parseInt
-  result.text = request.params.getOrDefault("text", "")
-
-routes:
-  get "/": resp @[Post()].dup(db.select("1")).index()
-  get "/posts/@id": resp Post().dup(db.select("id = ?", @"id")).render()
-  post "/posts":
-    resp BLANK_TWEET_FORM & request.parsePost.dup(db.insert).render()
-  put "/posts/@id": resp request.parsePost.dup(db.update).render()
-  delete "/posts/@id": resp request.parsePost.dup(db.delete).render()
-  get "/posts/edit/@id": resp Post().dup(db.select("id = ?", @"id")).renderEdit()
+run 8080
