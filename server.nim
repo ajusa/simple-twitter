ed7a724dae420b59a229ce2b774ddeacc2e5be6e
Arham Jain <arhamjain@gmail.com>
2023-07-30 20:16:10 -0700

    Migrate to nails autofill

--- server.nim
+++ server.nim
@@ -1,16 +1,19 @@
 import strutils, strformat
 import dekao, dekao/htmx, mummy, mummy/routers, webby, debby/sqlite, nails, nails/dekao_utils
 
-type Post* = ref object
-  id*: int
+type PostArgs = ref object of RootObj
   content*: string
+type Post* = ref object of PostArgs
+  id*: int
 
 let db = openDatabase(":memory:")
 db.createTable(Post)
+db.insert(Post(content: "hello"))
 
-proc getId(req: Request): int = req.query["id"].parseInt
+proc fromRequest*(req: Request, posts: var seq[Post]) = posts = db.filter(Post)
+proc fromRequest*(req: Request, post: var Post) = post = db.get(Post, req.query["id"].parseInt)
 
-proc form(post: Post) = 
+proc form(post: Post) =
   textarea: placeholder "Write here"; name "content"; say post.content
 
 proc show(post: Post) = form:
@@ -19,50 +22,53 @@ proc show(post: Post) = form:
   button: hxGet &"/posts/edit?id={post.id}"; say "Edit"
   button: hxDelete &"/posts?id={post.id}"; say "Delete"
 
-proc new(post: Post) = 
-  post.show()
-  tdiv("#newtweet"): 
-    hxSwapOob "true"
-    Post().form()
+proc index(req: Request, posts: seq[Post]) =
+  req.render:
+    html: lang "en"
+    head:
+      meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
+      script: src "https://unpkg.com/htmx.org@1.6.1"
+      title: say "Simple Twitter"
+    body:
+      tdiv "#posts":
+        for post in posts: post.show()
+      form:
+        tdiv("#newtweet"): Post().form()
+        button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
 
-proc edit(post: Post) = form:
-  post.form()
-  button: hxGet &"/posts?id={post.id}"; say "Cancel"
-  button: hxPatch &"/posts?id={post.id}"; say "Update"
+proc create(req: Request, args: PostArgs) =
+  var post = Post(content: args.content)
+  db.insert(post)
+  req.render:
+    post.show()
+    tdiv("#newtweet"):
+      hxSwapOob "true"
+      Post().form()
 
-proc index(posts: seq[Post]) =
-  html: lang "en"
-  head:
-    meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
-    script: src "https://unpkg.com/htmx.org@1.6.1"
-    title: say "Simple Twitter"
-  body:
-    tdiv "#posts":
-      for post in posts: post.show()
-    form:
-      tdiv("#newtweet"): Post().form()
-      button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
+proc edit(req: Request, post: Post) =
+  req.render: form:
+    post.form()
+    button: hxGet &"/posts?id={post.id}"; say "Cancel"
+    button: hxPut &"/posts?id={post.id}"; say "Update"
 
-proc indexHandler(req: Request): seq[Post] = db.filter(Post)
+proc update(req: Request, args: tuple[id: int, postArgs: PostArgs]) =
+  var post = Post(id: args.id, content: args.postArgs.content)
+  db.update(post)
+  req.render: post.show()
 
-proc upsertHandler(req: Request): Post =
-  result = Post(content: req.body.parseSearch["content"])
-  if "id" in req.query: result.id = req.getId()
-  db.upsert(result)
+proc show(req: Request, post: Post) = req.render: post.show()
 
-proc showHandler(req: Request): Post = db.get(Post, req.getId())
-
-proc deleteHandler(req: Request) =
-  db.delete(Post(id: req.getId()))
-  req.respond(200, body = "")
+proc delete(req: Request, post: Post) =
+  db.delete(post)
+  req.respond("")
 
 var router: Router
-router.get("/", indexHandler.renderWith(index))
-router.post("/posts", upsertHandler.renderWith(new))
-router.get("/posts/edit", showHandler.renderWith(edit))
-router.patch("/posts", upsertHandler.renderWith(show))
-router.get("/posts", showHandler.renderWith(show))
-router.delete("/posts", deleteHandler)
+router.get("/", fillArgs(index))
+router.post("/posts", fillArgs(create))
+router.get("/posts/edit", fillArgs(edit))
+router.put("/posts", fillArgs(update))
+router.get("/posts", fillArgs(show))
+router.delete("/posts", fillArgs(delete))
 
 let server = newServer(router)
 echo "Serving on http://localhost:8080"
