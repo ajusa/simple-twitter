2093ceb7d5deae4d1b7447858adbaf2b7d4c6ab5
Arham Jain <arhamjain@gmail.com>
2024-11-03 20:49:33 -0800

    Remove nails
    Turned out that it wasn't adding nearly enough functionality to justify the roughly 5 lines of code it saved.

--- server.nim
+++ server.nim
@@ -1,75 +1,80 @@
-import strutils, strformat 
-import dekao, dekao/htmx, mummy, mummy/routers, webby, debby/sqlite, nails, nails/dekao_utils
+import dekao, dekao/htmx, mummy, mummy/routers, webby, debby/sqlite
 
-type PostArgs = ref object of RootObj
-  content*: string
-type Post* = ref object of PostArgs
+type Post* = ref object
   id*: int
+  content*: string
 
 let db = openDatabase(":memory:")
 db.createTable(Post)
 db.insert(Post(content: "hello"))
 
-proc fromRequest*(req: Request, posts: var seq[Post]) = posts = db.filter(Post)
-proc fromRequest*(req: Request, post: var Post) = post = db.get(Post, req.query["id"].parseInt)
+proc postId(params: PathParams): int = params.getOrDefault("id", "0").parseInt
 
-proc form(post: Post) =
+proc toPost(params: PathParams, body: string): Post =
+  Post(id: params.postId(), content: body.parseSearch["content"])
+
+proc renderForm(post = Post()) =
   textarea: placeholder "Write here"; name "content"; say post.content
 
 proc show(post: Post) = form:
   hxTarget "this"
+  hxSwap "outerHTML"
   span: say post.content
-  button: hxGet &"/posts/edit?id={post.id}"; say "Edit"
-  button: hxDelete &"/posts?id={post.id}"; say "Delete"
+  button: hxGet "/posts/" & $post.id & "/edit"; say "Edit"
+  button: hxDelete "/posts/" & $post.id; say "Delete"
 
-proc index(req: Request, posts: seq[Post]) =
-  req.render:
-    html: lang "en"
-    head:
-      meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
-      script: src "https://unpkg.com/htmx.org@1.6.1"
-      title: say "Simple Twitter"
-    body:
-      tdiv "#posts":
-        for post in posts: post.show()
-      form:
-        tdiv("#newtweet"): Post().form()
-        button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
+proc edit(post: Post): string = render:
+  form:
+    hxTarget "this"
+    hxSwap "outerHTML"
+    post.renderForm()
+    button: hxGet "/posts/" & $post.id; say "Cancel"
+    button: hxPut "/posts/" & $post.id; say "Update"
 
-proc create(req: Request, args: PostArgs) =
-  var post = Post(content: args.content)
+proc index(posts: seq[Post]): string = render:
+  html: lang "en"
+  head:
+    meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
+    script: src "https://unpkg.com/htmx.org@1.6.1"
+    title: say "Simple Twitter"
+  body:
+    tdiv "#posts": 
+      for post in posts: post.show()
+    form:
+      tdiv("#newtweet"): renderForm()
+      button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
+
+proc create(post: Post): string = render:
+  post.show()
+  tdiv("#newtweet"):
+    hxSwapOob "true"
+    renderForm()
+
+using req: Request
+proc respond(req; resp: string) = req.respond(200, @[("Content-Type", "text/html")], resp)
+var router: Router
+router.get "/", proc (req) =
+  req.respond(db.filter(Post).index())
+
+router.post "/posts", proc (req) = 
+  let post = req.pathParams.toPost(req.body)
   db.insert(post)
-  req.render:
-    post.show()
-    tdiv("#newtweet"):
-      hxSwapOob "true"
-      Post().form()
+  req.respond(post.create())
 
-proc edit(req: Request, post: Post) =
-  req.render: form:
-    post.form()
-    button: hxGet &"/posts?id={post.id}"; say "Cancel"
-    button: hxPut &"/posts?id={post.id}"; say "Update"
+router.get "/posts/@id/edit", proc (req) =
+  req.respond(db.get(Post, req.pathParams.postId).edit())
 
-proc update(req: Request, args: tuple[id: int, postArgs: PostArgs]) =
-  var post = Post(id: args.id, content: args.postArgs.content)
+router.put "/posts/@id", proc (req) =
+  let post = req.pathParams.toPost(req.body)
   db.update(post)
-  req.render: post.show()
+  req.respond(block: render: post.show())
 
-proc show(req: Request, post: Post) = req.render: post.show()
+router.get "/posts/@id", proc (req) =
+  req.respond(block: render: db.get(Post, req.pathParams.postId).show())
 
-proc delete(req: Request, post: Post) =
-  db.delete(post)
+router.delete "/posts/@id", proc (req) =
+  db.delete(Post(id: req.pathParams.postId))
   req.respond("")
 
-var router: Router
-router.get("/", fillArgs(index))
-router.post("/posts", fillArgs(create))
-router.get("/posts/edit", fillArgs(edit))
-router.put("/posts", fillArgs(update))
-router.get("/posts", fillArgs(show))
-router.delete("/posts", fillArgs(delete))
-
-let server = newServer(router)
 echo "Serving on http://localhost:8080"
-server.serve(Port(8080))
+newServer(router).serve(Port(8080))
