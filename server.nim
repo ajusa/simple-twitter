24c3ec40c4158b81d34e6f1db63e3611782ddb19
Arham Jain <arhamjain@gmail.com>
2024-11-03 21:16:08 -0800

    Remove advanced usage of HTMX
    As this is a toy example, there's no real need to do fine grained updated to the DOM using HTMX. A standard HTML form is fine, and hx-boost can take care of any full page refresh avoidance.

--- server.nim
+++ server.nim
@@ -17,64 +17,61 @@ proc renderForm(post = Post()) =
   textarea: placeholder "Write here"; name "content"; say post.content
 
 proc show(post: Post) = form:
-  hxTarget "this"
-  hxSwap "outerHTML"
-  span: say post.content
-  button: hxGet "/posts/" & $post.id & "/edit"; say "Edit"
-  button: hxDelete "/posts/" & $post.id; say "Delete"
+  action "/posts/" & $post.id & "/delete"
+  tmethod "post"
+  span: say post.content & "&nbsp;"
+  a: href "/posts/" & $post.id & "/edit"; say "Edit"
+  button: say "Delete"
 
-proc edit(post: Post): string = render:
+proc edit(post: Post) = 
   form:
-    hxTarget "this"
-    hxSwap "outerHTML"
+    action "/posts/" & $post.id
+    tmethod "post"
     post.renderForm()
-    button: hxGet "/posts/" & $post.id; say "Cancel"
-    button: hxPut "/posts/" & $post.id; say "Update"
+    a: href "/"; say "Cancel"
+    button: say "Update"
 
-proc index(posts: seq[Post]): string = render:
+proc index(params: PathParams, posts: seq[Post]): string = render:
+  let id = params.getOrDefault("id", "-1").parseInt
   html: lang "en"
   head:
     meta: charset "UTF-8"; name "viewport"; content "width=device-width, initial-scale=1"
     script: src "https://unpkg.com/htmx.org@1.6.1"
     title: say "Simple Twitter"
   body:
-    tdiv "#posts": 
-      for post in posts: post.show()
+    hxBoost "true"
+    for post in posts:
+      if id >= 0 and post.id == id: post.edit()
+      else: post.show()
     form:
-      tdiv("#newtweet"): renderForm()
-      button: hxPost "/posts"; hxTarget "#posts"; hxSwap "beforeend"; say "Add"
-
-proc create(post: Post): string = render:
-  post.show()
-  tdiv("#newtweet"):
-    hxSwapOob "true"
-    renderForm()
+      action "/posts"
+      tmethod "post"
+      renderForm()
+      button: say "Add"
 
 using req: Request
 proc respond(req; resp: string) = req.respond(200, @[("Content-Type", "text/html")], resp)
+proc redirect(req; url: string) = req.respond(302, @[("Location", url)])
 var router: Router
 router.get "/", proc (req) =
-  req.respond(db.filter(Post).index())
+  req.respond(req.pathParams.index(db.filter(Post)))
 
 router.post "/posts", proc (req) = 
   let post = req.pathParams.toPost(req.body)
   db.insert(post)
-  req.respond(post.create())
+  req.redirect("/")
 
 router.get "/posts/@id/edit", proc (req) =
-  req.respond(db.get(Post, req.pathParams.postId).edit())
+  req.respond(req.pathParams.index(db.filter(Post)))
 
-router.put "/posts/@id", proc (req) =
+router.post "/posts/@id", proc (req) =
   let post = req.pathParams.toPost(req.body)
   db.update(post)
-  req.respond(block: render: post.show())
+  req.redirect("/")
 
-router.get "/posts/@id", proc (req) =
-  req.respond(block: render: db.get(Post, req.pathParams.postId).show())
-
-router.delete "/posts/@id", proc (req) =
+router.post "/posts/@id/delete", proc (req) =
   db.delete(Post(id: req.pathParams.postId))
-  req.respond("")
+  req.redirect("/")
 
 echo "Serving on http://localhost:8080"
 newServer(router).serve(Port(8080))
